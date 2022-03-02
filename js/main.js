38a48ea9c9795ebdbae17bec4aafb5e6e750dfc7
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-02 15:53:13 +0100

    Brain storming

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 const gridContainer = document.querySelector(".grid");
 
-for (let i = 1; i <= 100; i++) {
+/*for (let i = 1; i <= 100; i++) {
     
     const square = document.createElement("div");
     square.classList.add("square");
+    square.innerHTML = i;
+    gridContainer.append(square);
+}*/
+
+/*for (let i = 1; i <= 46; i++) {
+    
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = i;
+    gridContainer.append(square);
+}*/
+
+for (let i = 1; i <= 64; i++) {
+    
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = i;
     gridContainer.append(square);
 }
+
+// se seleziono bassa compare la griglia 6*6
+// se selezion media compare la griglia 8*8
+// se seleziono alta compare la griglia 10*10
+
